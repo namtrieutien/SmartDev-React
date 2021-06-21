0c800ab5a3ecd697ed355e9d75edfb68a5ea9719
leanhhuy103161 <leanhhuy103161@gmail.com>
2021-06-21 09:27:10 +0700

    fix: fix banner

--- src/components/Contents/Banner/index.jsx
+++ src/components/Contents/Banner/index.jsx
@@ -1,36 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import "./Banner.css"
+
 Banner.propTypes = {
   
 };
 
 function Banner(props) {
   return (
-    <div>
+    <div className="Banner">
       {/* <!-- Banner Starts Here --> */}
-    <div className="banner header-text">
-      <div className="owl-banner owl-carousel">
-        <div className="banner-item-01">
-          <div className="text-content">
-            <h4>Best Offer</h4>
-            <h2>New Arrivals On Sale</h2>
-          </div>
-        </div>
-        <div className="banner-item-02">
-          <div className="text-content">
-            <h4>Flash Deals</h4>
-            <h2>Get your best products</h2>
-          </div>
-        </div>
-        <div className="banner-item-03">
-          <div className="text-content">
-            <h4>Last Minute</h4>
-            <h2>Grab last minute deals</h2>
-          </div>
-        </div>
+      <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/images/slide_01.jpg" class="d-block w-100" alt="..." />
+      <div class="carousel-caption d-none d-md-block">
+        <h1>First slide label</h1>
+        <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, repudiandae.</h6>
+        <button class="btn-banner mt-3">Find more</button>
       </div>
     </div>
+    <div class="carousel-item">
+      <img src="assets/images/slide_02.jpg" class="d-block w-100" alt="..." />
+      <div class="carousel-caption d-none d-md-block">
+        <h1>Second slide label</h1>
+        <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, repudiandae.</h6>
+        <button class="btn-banner mt-3">Find more</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/slide_03.jpg" class="d-block w-100" alt="..." />
+      <div class="carousel-caption d-none d-md-block">
+        <h1>Third slide label</h1>
+        <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, repudiandae.</h6>
+        <button class="btn-banner mt-3">Find more</button>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
     {/* <!-- Banner Ends Here --> */}
     </div>
   );
