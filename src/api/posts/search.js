f497d6f1f096a884c262abb5aa16bcad8bd12307
namtt007 <nam.d02th@gmail.com>
2021-06-26 14:32:18 +0700

    - refactory: search

--- src/api/posts/search.js
+++ src/api/posts/search.js
@@ -4,5 +4,7 @@ import axios from 'axios';
 import apiSunny from '../sunny';
 
 export const getSearchPostData = (keywordSearch) => {
-    return apiSunny.get(`posts/search-post-by-title?title=` + keywordSearch + `&_page=0&_limit=6`);
+    const url = 'posts/search-post-by-title?title=' + keywordSearch +'\&_page=0\&_limit=0'; 
+    console.log(url);
+    return apiSunny.get(url);
 }
--- src/api/sunny.js
+++ src/api/sunny.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL : 'https://smartdev-sunny.herokuapp.com/'
+  //baseURL : 'https://smartdev-sunny.herokuapp.com/'
   baseURL: 'http://localhost:8080/'
 });
 
 instance.interceptors.request.use(
   (config) => {
-    config.headers.Authorization = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzaXRyYW5odWVAZ21haWwuY29tIiwiaWF0IjoxNjI0NTkxNDM1LCJleHAiOjE2MjQ2MzQ2MzV9.gn-Ll-EeTjTNZCD_ykEyj5aZFZ6lHZnag-fhkY9OfEKKDKjt-_XwTMrFMxT6zCRFArXUCldh7sGzYiLzHk5P8w`;
+    config.headers.Authorization = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuYW0xQGdtYWlsLmNvbSIsImlhdCI6MTYyNDY4MjI2MCwiZXhwIjoxNjI0NzI1NDYwfQ.V0cMISLYX5K2eC3UtXzuYgUPnQQCfZwsUN8s1PFRzFtgXqeecFAv5R9nGzhYEPdrIqqzrnt4Q3rzbmQT7b4SiA`;
     return config;
   }
 )
