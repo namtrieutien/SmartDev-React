690cc607368cb3226b16de3dde843feb06e766c0
Nguyen Huu Nam <nam.d02th@gmail.com>
2021-07-06 19:54:58 +0700

    - refactory: format product page

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -9,7 +9,7 @@ import PostList from '../../components/PostList';
 import Pagination from '../../components/Pagination';
 
 import { connect } from 'react-redux';
-import {searchLoading} from '../../redux/actions/posts/search.action';
+import { searchLoading } from '../../redux/actions/posts/search.action';
 import SearchNotFound from '../../components/SearchPosts/SearchNotFound';
 
 // Products.propTypes = {
@@ -33,14 +33,14 @@ function Products(props) {
   }
 
   useEffect(() => {
-    
+
     const params = {
       title: props.params.title,
       _page: filters._page,
       _limit: props.params._limit
     }
     props.searchPost(params);
-    if(props.error.code == 200){
+    if (props.error.code == 200) {
       setPostList(props.data);
       setPagination(props.pagination);
     }
@@ -49,7 +49,7 @@ function Products(props) {
   return (
     <div>
       <Header />
-      <Content postList={props.error.code == 200?props.data:{}} handlePageChange={handlePageChange} pagination={props.error.code == 200?props.pagination:null} />
+      <Content postList={props.error.code == 200 ? props.data : {}} handlePageChange={handlePageChange} pagination={props.error.code == 200 ? props.pagination : null} />
       {props.error.code != 200 && <SearchNotFound />}
       <Footer />
     </div>
@@ -61,42 +61,45 @@ class Content extends React.Component {
     return (
       <div>
         <div className="page-heading products-heading header-text">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="text-content">
-                  <h4>new arrivals</h4>
-                  <h2>sixteen products</h2>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="text-content">
+                    <h4>new arrivals</h4>
+                    <h2>sixteen products</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div>
+          <div className="products">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="filters">
+                    <ul>
+                      <li className="active" data-filter="*">All Products</li>
+                      <li data-filter=".des">Featured</li>
+                      <li data-filter=".dev">Flash Deals</li>
+                      <li data-filter=".gra">Last Minute</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
-        <div className="products">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="filters">
-                  <ul>
-                    <li className="active" data-filter="*">All Products</li>
-                    <li data-filter=".des">Featured</li>
-                    <li data-filter=".dev">Flash Deals</li>
-                    <li data-filter=".gra">Last Minute</li>
-                  </ul>
-                </div>
+        <PostList posts={this.props.postList} />
+        <div>
+          <div className="products">
+            <div className="container">
+              <div className="row">
+                {
+                  this.props.pagination && <Pagination handlePageChange={this.props.handlePageChange} pagination={this.props.pagination} />
+                }
               </div>
-             
-              <div className="col-md-12">
-                <div className="filters-content">
-                  <div className="row">
-                   <PostList posts={this.props.postList} />
-                  </div>
-                </div>
-              </div>
-              {
-                this.props.pagination && <Pagination handlePageChange={this.props.handlePageChange} pagination={this.props.pagination} /> 
-              }
             </div>
           </div>
         </div>
