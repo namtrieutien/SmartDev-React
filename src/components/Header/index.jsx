817bbe0950b5e7af438a64c5549ebb7f66e69217
leanhhuy103161 <leanhhuy103161@gmail.com>
2021-07-06 21:49:08 +0700

    fix: updated new navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,36 +10,42 @@ import CartBadge from "../Cart/CartBadge";
 function Header(props) {
   return (
     <div className="Header">
-      {/* <!-- Header --> */}
-      <header>
-        <nav className="navbar navbar-expand-lg bg-dark fixed-top">
-          <div className="container reponsive-header">
-            <a className="navbar-brand" href="/">
-              <h2>
-                Chợ <em>Tốt</em>
-              </h2>
-            </a>
-            <button
-              className="navbar-toggler reponsive-toggle"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarResponsive"
-              aria-controls="navbarResponsive"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <Navbar />
-            </div>
-            <CartBadge className=""/>
-            <div className="search-posts">
-              <SearchPosts />
-            </div>
-          </div>
-        </nav>
-      </header>
+      <header className="ftco-section">
+        <div className="fixed-top">
+          <div className="wrap">
+			<div className="container">
+				<div className="row justify-content-between">
+						<div className="col">
+							<p className="mb-0 phone"><span className="fa fa-phone"></span> <a href="#">+848 98 213 745</a></p>
+						</div>
+						<div className="col d-flex justify-content-end">
+							<div className="social-media">
+				    		<p className="mb-0 d-flex">
+				    			<a href="#" className="d-flex align-items-center justify-content-center"><span className="fab fa-facebook"><i className="sr-only">Facebook</i></span></a>
+				    			<a href="#" className="d-flex align-items-center justify-content-center"><span className="fab fa-twitter"><i className="sr-only">Twitter</i></span></a>
+				    			<a href="#" className="d-flex align-items-center justify-content-center"><span className="fab fa-instagram"><i className="sr-only">Instagram</i></span></a>
+				    			<a href="#" className="d-flex align-items-center justify-content-center"><span className="fab fa-dribbble"><i className="sr-only">Dribbble</i></span></a>
+				    		</p>
+			        </div>
+						</div>
+				</div>
+			</div>
+		</div>
+		<nav className="navbar navbar-expand-lg bg-light ftco-navbar-light" id="ftco-navbar">
+	    <div className="container">
+	    	<a className="navbar-brand" href="/">Chợ Tốt <span>Chợ người Việt</span></a>
+        <SearchPosts />
+	      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span className="fa fa-bars"></span> Menu
+	      </button>
+	      <div className="collapse navbar-collapse" id="ftco-nav">
+	        <Navbar />
+	      </div>
+	    </div>
+	  </nav>
+        </div>
+		
+    </header>
     </div>
   );
 }
--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -66,14 +66,14 @@ function Profile(props) {
   else return (
     <div>
       <Header />
-      <div className="container mt-5">
+      <div className="container" style={{marginTop: "140px"}}>
         <div className="main-body ">
           <div className="row gutters-sm ">
             <div className="col-md-4 mb-3">
               <div className="card">
                 <div className="card-body">
                   <div className="d-flex flex-column align-items-center text-center">
-                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width={150} />
+                    <img src="https://avatars.dicebear.com/api/bottts/sad.svg" alt="Admin" className="rounded-circle" width={150} />
                     <div className="mt-3">
                       <h4>{data.user.name}</h4>
                       <p className="text-secondary mb-1">Premium Member</p>
