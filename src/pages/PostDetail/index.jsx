78dadeb45a7c97a3a2a478e592e82337bb6d6993
sitranv <sitranhue@gmail.com>
2021-07-06 14:27:10 +0700

    post detail

--- src/pages/PostDetail/index.jsx
+++ src/pages/PostDetail/index.jsx
@@ -42,7 +42,8 @@ const mapDispatchToProps = (dispatch) => {
 
 function PostDetail(props) {
   const location = useLocation();
-  const { id, title, description, price, category } = location.state.post;
+  console.log(location.state.post);
+  const { id, title, description, price, category, image, user } = location.state.post;
   const discount = 15
   const price_discount = price * discount / 100
   const price_before = price + price_discount
@@ -84,6 +85,11 @@ function PostDetail(props) {
   });
 
   useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
     dispatch(getReportTypes())
   }, [])
 
@@ -117,27 +123,44 @@ function PostDetail(props) {
         <div className="single_product">
           <div className="container-fluid" style={{ backgroundColor: '#fff', padding: '11px' }}>
             <div className="row">
-              <div className="col-lg-4 order-lg-2 order-1 ml-5">
-                <div className="image_selected"><img src="https://i.imgur.com/K4b71NV.jpg" alt="post_img" /></div>
+              <div className="col-lg-4 order-lg-2 order-1 ml-3">
+                <div className="image_selected"><img src={image} alt="post_img" /></div>
               </div>
-              <div className="col-lg-6 order-3 ml-5">
-                <div className="product_description">
-                  <nav>
+              <div className="col-lg-6 order-3 ml-3">
+                <div className="product_description mt-3">
+                  {/* <nav>
                     <ol className="breadcrumb">
                       <li className="breadcrumb-item active">Home</li>
                       <li className="breadcrumb-item active">Products</li>
                       <li className="breadcrumb-item active">{category}</li>
                     </ol>
-                  </nav>
-                  <div className="product_name">⚡⚡{title}</div>
-                  <div className="product-rating"><span className="badge badge-success"><i className="fa fa-star" /> 4.5 Star</span> <span className="rating-review">35 Ratings &amp; 45 Reviews</span></div>
-                  <div> <span className="product_price"> {VNDformat(price)}</span> <strike className="product_discount"> <span style={{ color: 'black' }}>{VNDformat(price_before)}<span> </span></span></strike> </div>
-                  <div> <span className="product_saved">You Saved {discount}%:</span> <span style={{ color: 'black' }}>{VNDformat(price_discount)}<span> </span></span></div>
-                  <hr className="singleline" />
-                  <div> <span className="product_info">{description}<span><br />
-                    <span className="product_info">Nhanh tay bấm mua ngày để rinh ngay sản phẩm siêu hót siêu rẻ này thôi<span><br />
-                    </span></span></span></span></div>
+                  </nav> */}
+                  <div className="product_name">{title}</div>
+                  {/* <div className="product-rating"><span className="badge badge-success"><i className="fa fa-star" /> 4.5 Star</span> <span className="rating-review">35 Ratings &amp; 45 Reviews</span></div> */}
                   <div>
+                    <span className="product_price"> {VNDformat(price)}</span>
+                    {/* <strike className="product_discount">
+                      <span style={{ color: 'black' }}>{VNDformat(price_before)}
+                        <span></span>
+                      </span>
+                    </strike> */}
+                  </div>
+                  {/* <div>
+                    <span className="product_saved">You Saved {discount}%:</span>
+                    <span style={{ color: 'black' }}>{VNDformat(price_discount)}
+                      <span> </span>
+                    </span>
+                  </div> */}
+                  <hr className="singleline" />
+                  <div>
+                    <span className="product_info">{description}<span>
+                      <br />
+                      {/* <span className="product_info">Nhanh tay bấm mua ngày để rinh ngay sản phẩm siêu hót siêu rẻ này thôi<span>
+                        <br />
+                      </span></span> */}
+                    </span></span>
+                  </div>
+                  {/* <div>
                     <div className="row">
                       <div className="col-md-5">
                         <div className="br-dashed">
@@ -150,30 +173,34 @@ function PostDetail(props) {
                         </div>
                       </div>
                       <div className="col-md-7"> </div>
-                    </div>
-                    <div className="row" style={{ marginTop: '15px' }}>
+                    </div> */}
+                  {/* <div className="row" style={{ marginTop: '15px' }}>
                       <div className="col-xs-6" style={{ marginLeft: '15px' }}> <span className="product_options">Size</span><br /> <button className="btn btn-primary btn-sm">Small</button> <button className="btn btn-primary btn-sm">Medium</button> <button className="btn btn-primary btn-sm">Big</button> </div>
-                    </div>
-                  </div>
+                    </div> */}
+                  {/* </div> */}
                   <hr className="singleline" />
                   <div className="order_info d-flex flex-row">
                     <form action="#">
                     </form></div>
-                  <div className="row">
-                    <div className="col-xs-6" style={{ marginLeft: '13px' }}>
+                  <div className="row ml-">
+                    {/* <div className="col-xs-6" style={{ marginLeft: '13px' }}>
                       <div className="product_quantity"> <span>QTY: </span> <input id="quantity_input" type="text" pattern="[0-9]*" defaultValue={1} disabled />
                         <div className="quantity_buttons">
                           <div id="quantity_inc_button" className="quantity_inc quantity_control"><i className="fas fa-chevron-up" /></div>
                           <div id="quantity_dec_button" className="quantity_dec quantity_control"><i className="fas fa-chevron-down" /></div>
                         </div>
                       </div>
-                    </div>
+                    </div> */}
                     <div className="col-xs-6">
-                      <button className="btn btn-outline-primary mr-2" onClick={handleCartItemClick} >
-                        <img className="feather feather-globe mr-2 icon-inline" width="30" height="30" src={require(`../../components/Profile/img/cart.png`).default} alt="Cart" />{status}
-                      </button>
-                      <button type="button" className="btn btn-danger shop-button">Buy Now</button>
-                      <button type="button" className="btn btn-warning shop-button ml-2" data-toggle="modal" data-target="#reportModal">Report</button>
+                      <div className="col-12 col-md-6 d-flex">
+                        <button className="btn btn-outline-primary mr-2" onClick={handleCartItemClick} >
+                          <img className="feather feather-globe mr-2 icon-inline" width="30" height="30" src={require(`../../components/Profile/img/cart.png`).default} alt="Cart" />{status}
+                        </button>
+                        <button type="button" className="btn btn-danger shop-button">Buy Now</button>
+                      </div>
+                      <div className="col-12 col-md-6">
+                        <button type="button" className="btn btn-warning shop-button mt-2" data-toggle="modal" data-target="#reportModal">Report</button>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -189,51 +216,27 @@ function PostDetail(props) {
                 <table className="col-md-12">
                   <tbody>
                     <tr className="row mt-10">
-                      <td className="col-md-4"><span className="p_specification">Sales Package :</span> </td>
-                      <td className="col-md-8">
-                        <ul>
-                          <li>2 in 1 Laptop, Power Adaptor, Active Stylus Pen, User Guide, Warranty Documents</li>
-                        </ul>
+                      <td className="col-6 col-md-4"><span className="p_specification">Owner:</span> </td>
+                      <td className="col-6 col-md-8">
+                        {user.name}
                       </td>
                     </tr>
                     <tr className="row mt-10">
-                      <td className="col-md-4"><span className="p_specification">Model Number :</span> </td>
-                      <td className="col-md-8">
-                        <ul>
-                          <li> 14-dh0107TU </li>
-                        </ul>
+                      <td className="col-6 col-md-4"><span className="p_specification">Phone:</span> </td>
+                      <td className="col-6 col-md-8">
+                        {user.phone}
                       </td>
                     </tr>
                     <tr className="row mt-10">
-                      <td className="col-md-4"><span className="p_specification">Part Number :</span> </td>
-                      <td className="col-md-8">
-                        <ul>
-                          <li>7AL87PA</li>
-                        </ul>
+                      <td className="col-6 col-md-4"><span className="p_specification">Email:</span> </td>
+                      <td className="col-6 col-md-8">
+                        {user.email}
                       </td>
                     </tr>
                     <tr className="row mt-10">
-                      <td className="col-md-4"><span className="p_specification">Color :</span> </td>
-                      <td className="col-md-8">
-                        <ul>
-                          <li>Black</li>
-                        </ul>
-                      </td>
-                    </tr>
-                    <tr className="row mt-10">
-                      <td className="col-md-4"><span className="p_specification">Suitable for :</span> </td>
-                      <td className="col-md-8">
-                        <ul>
-                          <li>Processing &amp; Multitasking</li>
-                        </ul>
-                      </td>
-                    </tr>
-                    <tr className="row mt-10">
-                      <td className="col-md-4"><span className="p_specification">Processor Brand :</span> </td>
-                      <td className="col-md-8">
-                        <ul>
-                          <li>Intel</li>
-                        </ul>
+                      <td className="col-6 col-md-4"><span className="p_specification">Address:</span> </td>
+                      <td className="col-6 col-md-8">
+                        {user.address.commune}, {user.address.district}, {user.address.city}
                       </td>
                     </tr>
                   </tbody>
